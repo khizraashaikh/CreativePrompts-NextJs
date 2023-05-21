2be06fa43c821c74c3652b8d952b866fe65d7753
Khizr AA Shaikh <khizraashaikh@gmail.com>
2023-05-21 13:18:09 +0530

    Update & Delete Prompt Done.

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -24,8 +24,29 @@ const MyProfile = () => {
     if (session?.user.id) fetchPost();
   }, []);
 
-  const handleEdit = () => {};
-  const handleDelete = async () => {};
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
+
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   return (
     <Profile
